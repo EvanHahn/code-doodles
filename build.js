ffb8198a23e4576890054bce0c72b4f92b32c6ba
Evan Hahn <me@evanhahn.com>
2014-12-19 11:48:00 -0500

    Migrate to a custom build system

---
+++ build.js
@@ -0,0 +1,84 @@
+const copyFile = require('stream-copy-file');
+const fs = require('fs');
+const path = require('path');
+const rmdir = require('rmdir');
+const watch = require('watch').watchTree;
+const connect = require('connect');
+const serveStatic = require('serve-static');
+
+const watchOptions = {
+  ignoreDotfiles: true,
+  ignoreUnreadableDir: true
+};
+const thrower = function(err) { if (err) { throw err; } };
+
+const srcDir = path.resolve(__dirname, 'src');
+const srcFiles = fs.readdirSync(srcDir);
+const outputDir = path.resolve(__dirname, 'www');
+
+const recreateDir = function(path, callback) {
+  fs.exists(path, exists => {
+    if (exists) {
+      rmdir(path, err => {
+        if (err) {
+          callback(err);
+          return;
+        }
+        fs.mkdir(path, callback);
+      });
+    } else {
+      fs.mkdir(path, callback);
+    }
+  });
+};
+
+recreateDir(outputDir, err => {
+
+  srcFiles.forEach(file => {
+
+    const filename = path.resolve(srcDir, file);
+    fs.stat(filename, (err, stats) => {
+
+      if (err) { throw err; }
+
+      if (stats.isDirectory()) {
+
+        const buildFile = path.resolve(filename, 'build.js');
+
+        fs.exists(buildFile, exists => {
+
+          if (!exists) { return; }
+
+          const buildFunction = require(buildFile);
+          const outputFolder = path.resolve(outputDir, file);
+
+          const build = function build() {
+            console.log('building', file);
+            recreateDir(outputFolder, err => {
+              if (err) { throw err; }
+              buildFunction(outputFolder, thrower);
+            });
+          };
+
+          watch(filename, watchOptions, build);
+
+        });
+
+      } else {
+
+        const outputFilename = path.resolve(outputDir, file);
+        copyFile(filename, outputFilename, thrower);
+
+      }
+
+    });
+
+  });
+
+  const app = connect();
+  app.use(serveStatic(outputDir));
+  app.listen(3000, () => {
+    console.log('Code doodles are happening');
+  });
+
+});
