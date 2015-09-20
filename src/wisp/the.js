5a2015f63379990a380a159eb1e38b650d228188
Evan Hahn <me@evanhahn.com>
2015-09-19 22:18:34 -0700

    add comments and minor perf tweak

--- src/wisp/the.js
+++ src/wisp/the.js
@@ -1,35 +1,38 @@
 const Spectra = require('spectra');
 const makeCanvasFillScreen = require('./lib/make-canvas-fill-screen');
 
-const MAX_PARTICLES = 2048;
-
+// make a canvas
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 document.body.appendChild(canvas);
 makeCanvasFillScreen(canvas);
 
+// define particle data array
+const MAX_PARTICLES = 2048;
 const particleDataLength = MAX_PARTICLES * 3;
 const particleData = new Float64Array(particleDataLength);
 
-let lastTime;
+let lastTime = 0;
 function tick(t) {
-  const dt = lastTime == null ? 0 : t - lastTime;
+  // calculate ∆t
+  const dt = t - lastTime;
   lastTime = t;
 
+  // empty screen
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  // make a new particle
   const newX = (Math.random() * canvas.width) | 0;
-
   let i;
   for (i = 0; i < particleDataLength; i += 3) {
     const y = particleData[i + 1];
     if (y < 0) { break; }
   }
-
   particleData[i] = newX;
   particleData[i + 1] = canvas.height;
   particleData[i + 2] = Math.random();
 
+  // move and draw all the particles
   for (let i = 0; i < particleDataLength; i += 3) {
     const y = particleData[i + 1];
     if (y < 0) { continue; }
@@ -46,6 +49,7 @@ function tick(t) {
     ctx.fillRect(x, y | 0, size, size);
   }
 
+  // begin anew
   requestAnimationFrame(tick);
 }
 
